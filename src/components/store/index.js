c499cc31613803ba17b43cf8a6c4362eace069b1
sozani <sys612603@gmail.com>
2023-02-14 23:16:14 +0200

    export store

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -1,5 +1,6 @@
 import { configeStore } from "@reduxjs/toolkit";
 import uiSlice from "./ui-slice";
-configeStore({
+const store = configeStore({
   reducer: { ui: uiSlice.reducer },
 });
+export default store;
