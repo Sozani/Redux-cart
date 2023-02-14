13666e4fa384f1449b88d3eac8612d7c32f75d04
sozani <sys612603@gmail.com>
2023-02-14 23:51:42 +0200

    use dispatch actions

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -3,7 +3,9 @@ import { useDispatch } from "react-redux";
 import { uiActions } from "../store/ui-slice";
 const CartButton = (props) => {
   const dispatch = useDispatch();
-  const toggleCardHandler = () => {};
+  const toggleCardHandler = () => {
+    dispatch(uiActions.toggle());
+  };
   return (
     <button className={classes.button} onClick={toggleCardHandler}>
       <span>My Cart</span>
