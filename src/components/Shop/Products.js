ef9e1c46706ffadd0e0ca8d1a574470eeb0c967a
sozani <sys612603@gmail.com>
2023-02-16 22:13:50 +0200

    Adding DUMMY_PRODUCTS

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,15 +1,28 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
-
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    prize: 6,
+    title: "My First book",
+    description: "This is my first I wrote.",
+  },
+  {
+    id: "p2",
+    prize: 5,
+    title: "My second book",
+    description: "This is my second I wrote.",
+  },
+];
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
         <ProductItem
-          title='Test'
+          title="Test"
           price={6}
-          description='This is a first product - amazing!'
+          description="This is a first product - amazing!"
         />
       </ul>
     </section>
